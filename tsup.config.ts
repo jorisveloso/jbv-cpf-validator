fc479a23a5d40804d4b591cf250b880f847240af
Joris Veloso <jorisveloso@gmail.com>
2025-12-03 21:13:44 -0300

    fix: configure tsup to output .mjs for ESM

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
   clean: true,
   minify: false,
   treeshake: true,
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".js" : ".mjs",
+    };
+  },
 });
